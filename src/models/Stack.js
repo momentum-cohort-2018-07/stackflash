6653f811be3bfd9b1ae10eaddcfcc7dab179333c
Clinton Dreisbach <clinton@dreisbach.us>
2018-09-10 14:00:08 -0400

    Add get, create, update, and delete for individual stacks

--- src/models/Stack.js
+++ src/models/Stack.js
@@ -1,6 +1,7 @@
 import request from 'superagent'
 
 let userToken
+const apiDomain = process.env.REACT_APP_API_DOMAIN
 
 class Stack {
   constructor (properties) {
@@ -13,10 +14,59 @@ class Stack {
   }
 
   static all () {
-    return request.get(`${process.env.REACT_APP_API_DOMAIN}/api/stacks`)
+    return request.get(`${apiDomain}/api/stacks`)
       .set('Authorization', `Bearer ${userToken}`)
       .then(res => res.body.stacks)
       .then(stacksInfo => stacksInfo.map(s => new Stack(s)))
+  }
+
+  static get (id) {
+    return request.get(`${apiDomain}/api/stacks/${id}`)
+      .set('Authorization', `Bearer ${userToken}`)
+      .then(res => new Stack(res.body))
+  }
+
+  save () {
+    if (!this.id) {
+      return this.create()
+    } else {
+      return this.update()
+    }
+  }
+
+  create () {
+    return request.post(`${apiDomain}/api/stacks`)
+      .set('Authorization', `Bearer ${userToken}`)
+      .send({ title: this.title })
+      .then(res => res.body)
+      .then(stackInfo => {
+        Object.assign(this, stackInfo)
+        return this
+      })
+  }
+
+  update () {
+    return request.put(`${apiDomain}/api/stacks/${this.id}`)
+      .set('Authorization', `Bearer ${userToken}`)
+      .send({ title: this.title })
+      .then(res => res.body)
+      .then(stackInfo => {
+        Object.assign(this, stackInfo)
+        return this
+      })
+  }
+
+  delete () {
+    if (!this.id) { return false }
+    return request.delete(`${apiDomain}/api/stacks/${this.id}`)
+      .set('Authorization', `Bearer ${userToken}`)
+      .then(res => {
+        if (res.body.numDeleted > 0) {
+          return true
+        } else {
+          return false
+        }
+      })
   }
 }
 
--- src/models/Stack.test.js
+++ src/models/Stack.test.js
@@ -24,3 +24,71 @@ test('can retrieve list of stacks', (done) => {
     done()
   })
 })
+
+test('can create a stack', (done) => {
+  nock(process.env.REACT_APP_API_DOMAIN)
+    .post('/api/stacks', { title: 'New stack' })
+    .reply(201, {
+      id: 'bde',
+      title: 'New stack'
+    })
+
+  const stack = new Stack({ title: 'New stack' })
+  stack.save().then(stack => {
+    expect(stack.id).toBe('bde')
+    done()
+  })
+})
+
+test('can get a stack', (done) => {
+  nock(process.env.REACT_APP_API_DOMAIN)
+    .get('/api/stacks/bde')
+    .reply(200, {
+      id: 'bde',
+      title: 'Test stack'
+    })
+
+  Stack.get('bde').then(stack => {
+    expect(stack.id).toBe('bde')
+    expect(stack.title).toBe('Test stack')
+    done()
+  })
+})
+
+test('can update a stack', (done) => {
+  nock(process.env.REACT_APP_API_DOMAIN)
+    .get('/api/stacks/bde')
+    .reply(200, {
+      id: 'bde',
+      title: 'Test stack'
+    })
+
+  nock(process.env.REACT_APP_API_DOMAIN)
+    .put('/api/stacks/bde', { title: 'Updated title' })
+    .reply(200, {
+      id: 'bde',
+      title: 'Updated title'
+    })
+
+  Stack.get('bde').then(stack => {
+    stack.title = 'Updated title'
+    stack.save().then(stack => {
+      expect(stack.title).toBe('Updated title')
+      done()
+    })
+  })
+})
+
+test('can delete a stack', (done) => {
+  nock(process.env.REACT_APP_API_DOMAIN)
+    .delete('/api/stacks/bde')
+    .reply(200, {
+      numDeleted: 1
+    })
+
+  const stack = new Stack({ id: 'bde', title: 'Test stack' })
+  stack.delete().then(deleted => {
+    expect(deleted).toBe(true)
+    done()
+  })
+})
