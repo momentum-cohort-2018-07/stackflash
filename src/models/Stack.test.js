99a54747c9d45fe47f3db28de4d3caa4c4ab94e8
Clinton Dreisbach <clinton@dreisbach.us>
2018-09-10 13:26:27 -0400

    Fixed tests to use nock

--- src/models/Stack.test.js
+++ src/models/Stack.test.js
@@ -1,13 +1,26 @@
 /* globals test, expect */
 
 import Stack from './Stack'
+import nock from 'nock'
 
 Stack.setUserToken(process.env.REACT_APP_USER_TOKEN)
 
 test('can retrieve list of stacks', (done) => {
+  nock(process.env.REACT_APP_API_DOMAIN)
+    .get('/api/stacks')
+    .reply(200, {
+      stacks: [
+        {
+          id: 'abc',
+          title: 'Interview questions'
+        }
+      ]
+    })
+
   Stack.all().then(stacks => {
-    expect(stacks).not.toBeUndefined()
+    expect(stacks).toHaveLength(1)
     expect(stacks[0]).toBeInstanceOf(Stack)
+    expect(stacks[0].title).toBe('Interview questions')
     done()
   })
 })
