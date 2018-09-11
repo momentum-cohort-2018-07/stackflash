9591ca8671d9b7fc8131c7449627c540865404df
Clinton Dreisbach <clinton@dreisbach.us>
2018-09-11 15:13:43 -0400

    Set up node-sass to build Sass files for us
    *.css files are now considered build artifacts and we do not
    keep them in the repo.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bulma/css/bulma.css'
-import './index.css'
+import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
