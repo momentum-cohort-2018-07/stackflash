101e6be1e1834cf7ad24854787cce5c473f74d68
Clinton Dreisbach <clinton@dreisbach.us>
2018-09-12 23:08:08 -0400

    Minor CSS fixes

--- src/components/StackPage.js
+++ src/components/StackPage.js
@@ -20,21 +20,19 @@ class StackPage extends Component {
   render () {
     const stack = this.props.stack
     return (
-      <div className='board'>
-        <div style={{ margin: '15px' }}>
-          <div className='stackNav'>
-            <div className='stackTitle'><Title>{stack.title}</Title>
-              <button className='editTitleButton'>&#x270E;</button>
-              <button className='cancelTitleEditButton'>&#10006;</button></div>
-            <div className='edit-runDiv'><button className='editModeButton'>Edit</button><button className='runModeButton'>Run</button></div>
+      <div className='StackPage'>
+        <div className='stackNav'>
+          <div className='stackTitle'><Title>{stack.title}</Title>
+            <button className='editTitleButton'>&#x270E;</button>
+            <button className='cancelTitleEditButton'>&#10006;</button></div>
+          <div className='edit-runDiv'><button className='editModeButton'>Edit</button><button className='runModeButton'>Run</button></div>
+        </div>
+        <div className='FlashCardMiniDiv columns'>
+          <div className='column is-one-third'>
+            {stack.cards && stack.cards.map((card) => <FlashCardMini key={card.id} card={card} />)}
           </div>
-          <div className='FlashCardMiniDiv columns'>
-            <div className='column is-one-third'>
-              {stack.cards && stack.cards.map((card) => <FlashCardMini key={card.id} card={card} />)}
-            </div>
-            <div className='column is-one-third addCardButtonDiv'>
-              <button className='column is-one-third addCardButton'>+</button>
-            </div>
+          <div className='column is-one-third addCardButtonDiv'>
+            <button className='column is-one-third addCardButton'>+</button>
           </div>
         </div>
       </div>
