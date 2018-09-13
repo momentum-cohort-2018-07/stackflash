15f31810c90e503d02c899e0674fbfd81a71b73f
Clinton Dreisbach <clinton@dreisbach.us>
2018-09-12 23:07:58 -0400

    Add Font Awesome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bulma/css/bulma.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
