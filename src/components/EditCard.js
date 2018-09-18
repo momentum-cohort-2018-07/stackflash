894aaf3ffda7d9ec1ff1aa5e5e6e8a227a3ba2e2
Clinton Dreisbach <clinton@dreisbach.us>
2018-09-18 11:45:22 -0400

    Make editing save to API

--- src/components/EditCard.js
+++ src/components/EditCard.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react'
 import { Label, Input, Button } from 'bloomer'
+import PropTypes from 'prop-types'
 
 class EditCard extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      front: props.card.front,
+      back: props.card.back
+    }
+    this.onSave = this.onSave.bind(this)
+  }
+
+  onSave () {
+    const { card, onSave } = this.props
+
+    card.front = this.state.front
+    card.back = this.state.back
+    onSave(card)
+  }
+
   render () {
-    const { card } = this.props
+    const { front, back } = this.state
 
     return (
       <div className='FlashCardMini'>
         <div>edit</div>
         <div className='cardFront'>
           <div className='miniCard'>
-            <Label> Question
-              <Input className='question' value={card.front} />
+            <Label>Front
+              <Input value={front} onChange={event => this.setState({ front: event.target.value })} />
             </Label>
-            <Label>Answer
-              <Input className='answer' value={card.back} />
+            <Label>Back
+              <Input value={back} onChange={event => this.setState({ back: event.target.value })} />
             </Label>
-            <Button className='button save'>Save</Button>
-            <Button className='button cancel'>Cancel</Button>
+            <Button className='button save' onClick={this.onSave}>Save</Button>
+            <Button className='button cancel' onClick={this.props.onCancel}>Cancel</Button>
           </div>
         </div>
       </div>
     )
   }
+}
+
+EditCard.propTypes = {
+  card: PropTypes.shape({
+    front: PropTypes.string,
+    back: PropTypes.string
+  }).isRequired,
+  onSave: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired
 }
 
 export default EditCard
